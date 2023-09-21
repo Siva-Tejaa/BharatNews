c7269dac0403c0f83b18888a82d9a2acb8643a41
Siva-Tejaa <asivateja1999@gmail.com>
2023-09-21 19:13:42 +0530

    bharatnews

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./App.css";
 
 import Header from './components/Header/Header';
+import Search from './components/Search/Search';
 
 // import {Routes , Route} from 'react-router-dom';
 
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <div>
       <Header/>
+      <Search/>
     </div>
   )
 }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import SearchIcon from "../../assets/SearchIcon.png"
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='bg-[var(--background-color)] p-1 py-4 laptop:hidden'>
+        <form className='flex items-center justify-center '>
+            <input type="search" placeholder="Search Bharat News..." className='w-72 p-1 py-[5px] text-black outline-none rounded-l-sm caret-[#011E29] border-[0.5px] border-solid border-[#011E29] tablet:w-[22rem]'/>
+            <button className='bg-[#011E29] pb-[6px] pt-[5px] px-3 border-none rounded-r-sm'><img src={SearchIcon} className='w-5'/></button>
+        </form>
+    </div>
   )
 }
 
