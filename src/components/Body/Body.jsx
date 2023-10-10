a8eeb8affa5bd1a3ca9f6e49b45210f1009b4fa8
Siva-Tejaa <asivateja1999@gmail.com>
2023-10-10 19:08:46 +0530

    bharatnews new api updated

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -4,7 +4,10 @@ import TopHeadlines from '../TopHeadlines/TopHeadlines'
 const Body = () => {
   return (
     <div className='bg-[var(--background-color)] p-1 text-[14px] tablet:flex justify-center laptop:p-2  '>
-      <TopHeadlines/>
+      <div className="flex items-center flex-col">
+      <marquee className='items-center text-lg font-bold pt-2 pb-2 text-red-400'>Under Development</marquee>
+        <TopHeadlines/>
+      </div>
     </div>
   )
 }
--- src/components/TopHeadlines/TopHeadlines.jsx
+++ src/components/TopHeadlines/TopHeadlines.jsx
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from 'react';
-import {TOP_HEADLINES_IN_WORLD, FILTERS} from "../utils/Api.jsx"
+// import {} from "../utils/Api.jsx"
 
 const TopHeadlines = () => {
 
@@ -8,7 +8,7 @@ const TopHeadlines = () => {
   const[loading, setLoading] = useState(false);
 
   const fetchTopHeadlines = async () => {
-    await fetch(TOP_HEADLINES_IN_WORLD + sort + FILTERS)
+    await fetch("https://gnews.io/api/v4/top-headlines?category=world&lang=en&apikey=e042a1c3528b33266346284b0435abfb")
      .then((res) => res.json())
      .then((json) => {
       setHeadlines(json.articles);
@@ -41,7 +41,7 @@ const TopHeadlines = () => {
                 headlines?.map((headline, index) => (
                     <div key={index} className='bg-white shadow-[0_0_6px_0_rgba(121,121,121,0.3)] my-5 p-4 rounded-md hover:shadow-[0_0_10px_0_rgba(121,121,121,0.3)] tablet:w-[475px] laptop:w-[600px]'>
                         <div className='text-[16px] font-semibold tit-desc'>{headline?.title}</div>
-                        <img src={headline?.urlToImage} alt="Bharat News" className='rounded-sm'/>
+                        <img src={headline?.image} alt="Bharat News" className='rounded-sm'/>
                     </div>
                 ))
             }
