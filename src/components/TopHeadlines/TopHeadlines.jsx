eac5b9351d4162781d37fa5ee722da07254b4950
Siva-Tejaa <asivateja1999@gmail.com>
2023-10-26 14:36:01 +0530

    news card updated

--- src/components/TopHeadlines/TopHeadlines.jsx
+++ src/components/TopHeadlines/TopHeadlines.jsx
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import NoImageFound from "../../assets/NoImageFound.png";
+import moment from 'moment'
 // import {TOP_HEADLINES_IN_WORLD} from "../utils/Api.jsx"
 
 const TopHeadlines = () => {
@@ -43,12 +44,12 @@ const TopHeadlines = () => {
             {
                 headlines?.map((headline, index) => (
                     <div key={index} className='bg-white shadow-[0_0_6px_0_rgba(121,121,121,0.3)] my-5 p-2 rounded-md hover:shadow-[0_0_10px_0_rgba(121,121,121,0.3)] tablet:w-[475px] laptop:w-[750px] laptop:flex gap-3'>
-                        <img src={headline?.image ? headline?.image : NoImageFound} alt="Bharat News" className='rounded-sm bg-slate-50 items-center border border-solid border-[grey] min-w-[100%] max-w-[100%] min-h-[15rem] max-h-[15rem] laptop:min-w-[18rem] laptop:max-w-[18rem] laptop:min-h-[11rem] laptop:max-h-[11rem]'/>
-                        <div>
-                            <div className='text-[16px] font-semibold text-justify tit-desc'><a target='_blank' href={headline?.url}>{headline?.title}</a></div>
-                            <p className="text-justify content-desc">{headline?.content}</p>
-                            <p>Source : <a target='_blank' href={headline?.source?.url}>{headline?.source?.name}</a></p>
-                            <p>Published At : {headline?.publishedAt}</p>
+                        <a target='_blank' href={headline?.url}><img src={headline?.image ? headline?.image : NoImageFound} alt="Bharat News" className='rounded-sm bg-slate-50 items-center border border-solid border-[#a1a0a0] min-w-[100%] max-w-[100%] min-h-[15rem] max-h-[15rem] laptop:min-w-[18rem] laptop:max-w-[18rem] laptop:min-h-[11rem] laptop:max-h-[11rem]'/></a>
+                        <div className='flex flex-col gap-[3px]'>
+                            <div className='text-[16px] font-semibold text-justify tit-desc leading-tight hover:underline'><a target='_blank' href={headline?.url}>{headline?.title}</a></div>
+                            <p className="text-justify content-desc">{headline?.content.split("...")[0]}<a target='_blank' href={headline?.url}>...</a></p>
+                            <p className='text-[#757575] text-[13px]'>Source : <a className='underline hover:text-[var(--main-color)]' target='_blank' href={headline?.source?.url}>{headline?.source?.name}</a></p>
+                            <p className='text-[#757575] text-[13px]'>Published At : {moment.utc(headline?.publishedAt).format('hh:mm A')} on {moment.utc(headline?.publishedAt).format('dddd, D MMMM, YYYY')}</p>
                         </div>
                         
                     </div>
