69c1c0040a5448b1f047856404b6436766df0f43
Siva-Tejaa <asivateja1999@gmail.com>
2023-10-26 18:58:17 +0530

    skeleton added

--- src/components/TopHeadlines/TopHeadlines.jsx
+++ src/components/TopHeadlines/TopHeadlines.jsx
@@ -1,22 +1,25 @@
 import React,{useState, useEffect} from 'react';
 import NoImageFound from "../../assets/NoImageFound.png";
-import moment from 'moment'
+import moment from 'moment';
+import Skeleton, {SkeletonTheme} from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'
 // import {TOP_HEADLINES_IN_WORLD} from "../utils/Api.jsx"
 
 const TopHeadlines = () => {
 
-  const[sort, setSort] = useState("publishedAt")
+  const[sort, setSort] = useState("publishedAt");
+  const[type, setType] = useState("");
   const [headlines, setHeadlines] = useState([]);
   const[loading, setLoading] = useState(false);
 
   const fetchTopHeadlines = async () => {
-    await fetch("https://gnews.io/api/v4/top-headlines?category=world&lang=en&apikey=e042a1c3528b33266346284b0435abfb")
+    await fetch("https://gnews.io/api/v4/top-headlines?category=world&lang=en&apikey=53cf98e7e20debf6881f95eeadb90f18")
      .then((res) => res.json())
      .then((json) => {
       setHeadlines(json.articles);
       setLoading(false);
 
-      console.log(headlines)
+    //   console.log(headlines)
      })
      .catch((err) => console.log(err));
 
@@ -41,8 +44,30 @@ const TopHeadlines = () => {
             </div>
         </div>
         <div>
-            {
-                headlines?.map((headline, index) => (
+
+        {
+            loading ? 
+
+            <>
+             {
+                Array(3).fill("").map((arr,index) =>(
+                  <div key={index} className='bg-white shadow-[0_0_6px_0_rgba(121,121,121,0.3)] my-5 p-2 rounded-md hover:shadow-[0_0_10px_0_rgba(121,121,121,0.3)] flex flex-col gap-1 w-[340px] tablet:w-[475px] laptop:w-[750px] laptop:flex laptop:flex-row'>
+                     <Skeleton className='rounded-sm items-center border border-solid  min-w-[100%] max-w-[100%] min-h-[12rem] max-h-[12rem] laptop:min-w-[18rem] laptop:max-w-[18rem] laptop:min-h-[11rem] laptop:max-h-[11rem]'/>
+                     <div className='flex flex-col gap-1 laptop:gap-[6px]'>
+                       <Skeleton className='laptop:w-[436px] h-9'/>
+                       <Skeleton />
+                       <Skeleton />
+                       <Skeleton />
+                       <Skeleton />
+                       <Skeleton />
+                     </div>                 
+                  </div>
+                ))
+             }
+            </> : 
+
+            
+             headlines?.map((headline, index) => (
                     <div key={index} className='bg-white shadow-[0_0_6px_0_rgba(121,121,121,0.3)] my-5 p-2 rounded-md hover:shadow-[0_0_10px_0_rgba(121,121,121,0.3)] tablet:w-[475px] laptop:w-[750px] laptop:flex gap-3'>
                         <a target='_blank' href={headline?.url}><img src={headline?.image ? headline?.image : NoImageFound} alt="Bharat News" className='rounded-sm bg-slate-50 items-center border border-solid border-[#a1a0a0] min-w-[100%] max-w-[100%] min-h-[15rem] max-h-[15rem] laptop:min-w-[18rem] laptop:max-w-[18rem] laptop:min-h-[11rem] laptop:max-h-[11rem]'/></a>
                         <div className='flex flex-col gap-[3px]'>
@@ -53,8 +78,10 @@ const TopHeadlines = () => {
                         </div>
                         
                     </div>
-                ))
-            }
+            ))
+            
+        }
+
         </div>
     </div>
   )
